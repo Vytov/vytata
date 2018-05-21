ecfc0ce9120442d037fec4af7cfcb7749a713812
Ivan <ivan.vytov@gmail.com>
2018-05-21 11:02:34 +0300

    lala

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new SortedDictionary<double, int>();
+
+            int[] numberinput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+            foreach (var result in numberinput)
+            {
+                if (numbers.ContainsKey(result))
+                {
+
+                    numbers[result]++;
+                }
+                else {
+
+
+                    numbers.Add(result, 1);
+                
+                }
+            }
+         
+
+           
+        }
+    }
+}
